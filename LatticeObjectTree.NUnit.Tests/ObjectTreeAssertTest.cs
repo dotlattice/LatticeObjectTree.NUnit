43f2b82cce67e8e6beb9ce84b9a7fc9dd88a5b7d
dotlattice <dotlattice@users.noreply.github.com>
2016-04-10 10:22:45 -0500

    Update core library reference

--- LatticeObjectTree.NUnit.Tests/ObjectTreeAssertTest.cs
+++ LatticeObjectTree.NUnit.Tests/ObjectTreeAssertTest.cs
@@ -55,7 +55,7 @@ namespace LatticeObjectTree.NUnit
 
             var expectedMessage = "  Expected: <LatticeObjectTree.ObjectTree> with root <{ id = 1, message = hello }>" + Environment.NewLine
  + "  But was:  <LatticeObjectTree.ObjectTree> with root <{ id = 2, message = world }>" + Environment.NewLine
- + "  2 Differences:    < <<root>.id: expected value \"1\" but was \"2\".>, <<root>.message: expected value \"hello\" but was \"world\".> >";
+ + "  2 Differences:    < <<root>.id: expected value 1 but was 2.>, <<root>.message: expected value \"hello\" but was \"world\".> >";
             Assert.AreEqual(expectedMessage, expectedException.Message);
         }
 
@@ -68,7 +68,7 @@ namespace LatticeObjectTree.NUnit
 
             var expectedMessage = "  Expected: <LatticeObjectTree.ObjectTree> with root <LatticeObjectTree.NUnit.ObjectTreeAssertTest+Company>" + Environment.NewLine
  + "  But was:  <LatticeObjectTree.ObjectTree> with root <LatticeObjectTree.NUnit.ObjectTreeAssertTest+Company>" + Environment.NewLine
- + "  2 Differences:    < <<root>.Id: expected value \"1\" but was \"2\".>, <<root>.Name: expected value \"hello\" but was \"world\".> >";
+ + "  2 Differences:    < <<root>.Id: expected value 1 but was 2.>, <<root>.Name: expected value \"hello\" but was \"world\".> >";
             Assert.AreEqual(expectedMessage, expectedException.Message);
         }
 
@@ -81,7 +81,7 @@ namespace LatticeObjectTree.NUnit
 
             var expectedMessage = "  Expected: <LatticeObjectTree.ObjectTree> with root < 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25 >" + Environment.NewLine
                 + "  But was:  <LatticeObjectTree.ObjectTree> with root < 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26 >" + Environment.NewLine
-                + "  26 Differences:    < <<root>[0]: expected value \"0\" but was \"1\".>, <<root>[1]: expected value \"1\" but was \"2\".>, <<root>[2]: expected value \"2\" but was \"3\".>, <<root>[3]: expected value \"3\" but was \"4\".>, <<root>[4]: expected value \"4\" but was \"5\".>, <<root>[5]: expected value \"5\" but was \"6\".>, <<root>[6]: expected value \"6\" but was \"7\".>, <<root>[7]: expected value \"7\" but was \"8\".>, <<root>[8]: expected value \"8\" but was \"9\".>, <<root>[9]: expected value \"9\" but was \"10\".>, <<root>[10]: expected value \"10\" but was \"11\".>, <<root>[11]: expected value \"11\" but was \"12\".>, <<root>[12]: expected value \"12\" but was \"13\".>, <<root>[13]: expected value \"13\" but was \"14\".>, <<root>[14]: expected value \"14\" but was \"15\".>, <<root>[15]: expected value \"15\" but was \"16\".>, <<root>[16]: expected value \"16\" but was \"17\".>, <<root>[17]: expected value \"17\" but was \"18\".>, <<root>[18]: expected value \"18\" but was \"19\".>, <<root>[19]: expected value \"19\" but was \"20\".>... >";
+                + "  26 Differences:    < <<root>[0]: expected value 0 but was 1.>, <<root>[1]: expected value 1 but was 2.>, <<root>[2]: expected value 2 but was 3.>, <<root>[3]: expected value 3 but was 4.>, <<root>[4]: expected value 4 but was 5.>, <<root>[5]: expected value 5 but was 6.>, <<root>[6]: expected value 6 but was 7.>, <<root>[7]: expected value 7 but was 8.>, <<root>[8]: expected value 8 but was 9.>, <<root>[9]: expected value 9 but was 10.>, <<root>[10]: expected value 10 but was 11.>, <<root>[11]: expected value 11 but was 12.>, <<root>[12]: expected value 12 but was 13.>, <<root>[13]: expected value 13 but was 14.>, <<root>[14]: expected value 14 but was 15.>, <<root>[15]: expected value 15 but was 16.>, <<root>[16]: expected value 16 but was 17.>, <<root>[17]: expected value 17 but was 18.>, <<root>[18]: expected value 18 but was 19.>, <<root>[19]: expected value 19 but was 20.>... >";
             Assert.AreEqual(expectedMessage, expectedException.Message);
         }
 
